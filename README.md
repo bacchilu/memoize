c1d16007a0103bb1c7bf330e3cbfc6e913f47c1a
Luca Bacchi <bacchilu@gmail.com>
2017-11-30 17:19:44 +0100

    expire argument 	modified:   README.md 	modified:   index.js

--- README.md
+++ README.md
@@ -10,13 +10,18 @@ We can use this decorator function in this way:
         return fib(n - 1) + fib(n - 2);
     };
 
-
-    console.log(fib(40));
     console.log(fib(40));
 
 
     const fib2 = memoize(fib);
 
     console.log(fib2(40));
-    console.log(fib2(40));
-    console.log(fib2(40));
+
+We can also use an optionally _expire_ args, in milliseconds.
+
+    const fib3 = memoize(fib, 5000);
+
+    console.log(fib3(42));
+    setTimeout(function () {
+        console.log(fib3(42));
+    }, 6000);
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const memoize = function (f) {
+const memoize = function (f, expire=null) {
     const memoize = function () {
         const cache = memoize.cache;
         const key = JSON.stringify(arguments);
+        if (cache[key] !== undefined && expire !== null && (new Date().getTime()) > (cache[key]['ts'] + expire))
+          delete cache[key];
         if (cache[key] === undefined)
-            cache[key] = f.apply(this, arguments);
-        return cache[key];
+            cache[key] = {value: f.apply(this, arguments), ts: new Date().getTime()};
+        return cache[key]['value'];
     };
     memoize.cache = {};
     return memoize;
